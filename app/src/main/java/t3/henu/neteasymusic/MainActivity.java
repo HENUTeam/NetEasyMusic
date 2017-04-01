bc3882a0f1ea09009cd07d1000f31f62bc7cb209
枯瑜 <1468165899@qq.com>
2017-04-01 18:38:13 +0800

    2017/4/1 18:37

---
+++ app/src/main/java/t3/henu/neteasymusic/MainActivity.java
@@ -0,0 +1,13 @@
+package t3.henu.neteasymusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {//、
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
