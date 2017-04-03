5850dde9ec8ebcab71c26b97fb84f2dbe0173b87
枯瑜 <1468165899@qq.com>
2017-04-03 21:58:56 +0800

    2017/4/3 21:58 侧滑栏菜单的实现

--- app/src/main/java/t3/henu/neteasymusic/MainActivity.java
+++ app/src/main/java/t3/henu/neteasymusic/MainActivity.java
@@ -3,22 +3,31 @@ package t3.henu.neteasymusic;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.*;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import t3.henu.neteasymusic.appMain.MyViewPagerAdapter;
 import t3.henu.neteasymusic.appMain.TabLayout_Mid;
 import t3.henu.neteasymusic.appMain_left.LogInactivity;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
@@ -26,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private List<Fragment> fragments=new ArrayList<Fragment>();
     private ConstraintLayout constraintLayout_left;
     private DrawerLayout drawerLayout;
-
+    private ListView appmain_drawerlayout_left_listview;
+    private  String []menuNames;
+    private int []menuicons;
+    private List<Map<String,Object> > appmain_drawerlayout_left_listview_Data=null;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +47,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        initToolbar();initViewPager();
-        initDrawerlayout();
+
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                menuicons=new int[]{R.drawable.icon_appmain_left_menu_01,R.drawable.icon_appmain_left_menu_02,
+                        R.drawable.icon_appmain_left_menu_03,R.drawable.icon_appmain_left_menu_02
+                        ,R.drawable.icon_appmain_left_menu_02,R.drawable.icon_appmain_left_menu_02,
+                        R.drawable.icon_appmain_left_menu_02,R.drawable.icon_appmain_left_menu_02
+                        ,R.drawable.icon_appmain_left_menu_02,R.drawable.icon_appmain_left_menu_02,
+                        R.drawable.icon_appmain_left_menu_02,R.drawable.icon_appmain_left_menu_02
+                        ,R.drawable.icon_appmain_left_menu_02,R.drawable.icon_appmain_left_menu_02};
+                initToolbar();initViewPager();
+                initDrawerlayout();
+                drawerLayout= (DrawerLayout) findViewById(R.id.id_appmain_drawelayout);
+            }
+        }) ;
     }
 
     private void initDrawerlayout() {
         constraintLayout_left=(ConstraintLayout)findViewById(R.id.id_appmain_drawelayout_left);
-        drawerLayout= (DrawerLayout) findViewById(R.id.id_appmain_drawelayout);
         ImageButton btn= (ImageButton) findViewById(R.id.id_appmain_btn_showleft);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +84,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        initDrawerlayout_left_RecyclerView();
+    }
+
+    private void initDrawerlayout_left_RecyclerView() {
+        appmain_drawerlayout_left_listview= (ListView) findViewById(R.id.id_appmain_drawelayout_left_recyclerview1);
+        menuNames=getResources().getStringArray(R.array.appmain_drawerlayout_left_menu_itemNames);
+       appmain_drawerlayout_left_listview_Data=new ArrayList<Map<String,Object>>();
+        if(appmain_drawerlayout_left_listview_Data!=null){
+            for(int i=0;i<menuNames.length;i++){
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("image", menuicons[i]);               //图片资源
+                map.put("text", menuNames[i]); //物品详情
+                appmain_drawerlayout_left_listview_Data.add(map);
+            }
+        }
+        SimpleAdapter simpAdapter=new SimpleAdapter(this,appmain_drawerlayout_left_listview_Data,R.layout.appmain_drawerlayout_left_recycleview_itemview,
+                new String[]{"image","text"}, new int[]{R.id.id_appmain_drawelayout_left_recyclerview1_item_imageview
+                ,R.id.id_appmain_drawelayout_left_recyclerview1_item_textview});
+       appmain_drawerlayout_left_listview.setAdapter(simpAdapter);
     }
 
     private void initToolbar() {
