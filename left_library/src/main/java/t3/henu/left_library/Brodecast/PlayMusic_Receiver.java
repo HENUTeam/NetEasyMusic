1754e5e535b13931a396f5a53f792ae4a350a281
枯瑜 <1468165899@qq.com>
2017-05-19 16:32:09 +0800

    2017/5/19 测试合并

--- left_library/src/main/java/t3/henu/left_library/Brodecast/PlayMusic_Receiver.java
+++ left_library/src/main/java/t3/henu/left_library/Brodecast/PlayMusic_Receiver.java
@@ -3,18 +3,26 @@ package t3.henu.left_library.Brodecast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
-import static t3.henu.left_library.MainActivity.play_status;
+import t3.henu.left_library.Activities.*;
+import t3.henu.left_library.MainActivity;
+import t3.henu.left_library.R;
 
 public class PlayMusic_Receiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        play_status status= (play_status) intent.getExtras().get("play_status");
-        switch (status){
-            case IS_PLAY:
-                
-                break;
+        Bundle bun=intent.getBundleExtra("Bunde");
+        SongInfo songInfo= (SongInfo) bun.getSerializable("songinfo");
+        MainActivity.t_singer.setText(songInfo.singer);
+        MainActivity.t_songname.setText(songInfo.getSong());
+        boolean status= (boolean) intent.getExtras().get("play_status");
+        if(status==false){
+            MainActivity.btn_play.setImageResource(R.drawable.icon_play1);
+        }else{
+            MainActivity.btn_play.setImageResource(R.drawable.icon_pause);
         }
+       // Toast.makeText(context,"收到广播："+(status),Toast.LENGTH_LONG).show();
     }
 }
